6da97ad4f51d14ec18f01885835ae82fc9c185fc
Lali346 <lagartopiero@gmail.com>
2021-10-19 21:28:50 -0300

    no le presten atencion al nombre Xd

---
+++ Java select a file/src/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+
+    public static void main(String[] args) {
+
+        // JFileChooser = A GUI mechanism that let's a user choose a file (helpful for opening or saving files)
+
+        new MyFrame();
+    }
+}
---
+++ Java textfield/src/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+
+    public static void main(String[] args) {
+
+
+        // JTextField = A GUI textbox component that can be used to add, set, or get text
+
+        new MyFrame();
+    }
+}
